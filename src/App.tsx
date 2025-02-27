f54ec54a9129ad484caa242dc541be0515fddbed
Javi <javierezpeleta@gmail.com>
2025-02-27 13:19:45 +0100

    removing privy from the extension... we'll handle auth differently (from thewebsite repo)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from "react";
-import PrivyLogin from "./components/PrivyLogin";
-import UserDebugger from "./components/UserDebugger";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
-import { usePrivy } from "@privy-io/react-auth";
-
-type UserSession = {
-  id: string;
-  email: string;
-  name: string;
-  image: string;
-};
+import { useEffect, useState } from "react"
 
 function App() {
-  const { user } = usePrivy();
-  const [count, setCount] = useState(0);
-  const [userSession, setUserSession] = useState<UserSession | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 200)
+  }, [])
 
   return (
     <div className="w-[500px] h-[500px] bg-purple-300 hello black mx-auto border-2 border-black p-4">
       {/* <UserDebugger /> */}
 
       {isLoading && <div>Loading...</div>}
-      {userSession && <div>User session found</div>}
-      {!userSession && <PrivyLogin />}
+      <div>YOOOOO!!</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-import { PrivyProvider } from "@privy-io/react-auth";
-
 import App from "./App";
-import { PRIVY_APP_ID } from "./lib/constants";
 const root = document.getElementById("root");
 if (!root) {
   throw new Error("Root element not found");
@@ -22,24 +19,6 @@ const rootElement = ReactDOM.createRoot(root);
 
 rootElement.render(
   <React.StrictMode>
-    <PrivyProvider
-      appId={PRIVY_APP_ID}
-      config={{
-        // Display email as only login method
-        loginMethods: ["email"],
-        // Customize Privy's appearance in your app
-        appearance: {
-          theme: "light",
-          accentColor: "#676FFF",
-          logo: "https://javitoshi.com/images/lingolin.png",
-        },
-        // Create embedded wallets for users who don't have a wallet
-        embeddedWallets: {
-          createOnLogin: "users-without-wallets",
-        },
-      }}
-    >
-      <App />
-    </PrivyProvider>
+    <App />
   </React.StrictMode>
 );
