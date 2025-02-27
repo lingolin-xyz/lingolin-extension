ddadadb9d19394dff380236a246a9b2d9001ccdd
Javi <javierezpeleta@gmail.com>
2025-02-27 14:05:18 +0100

    injecting a thing so the frontend website knows if user has the extension already installed, lfg

--- public/content.js
+++ public/content.js
@@ -1,1 +1,23 @@
-console.log("Hello from content script");
+console.log(" 🍎 ->->-> Hello from content script")
+
+// window.addEventListener("message", (event) => {
+//   if (event.data?.type === "EXTENSION_CHALLENGE") {
+//     // Sign the challenge (use a private key stored in extension)
+//     const signature = signChallenge(event.data.challenge)
+//     console.log(" 🍎 -> signature", signature)
+//     window.postMessage(
+//       {
+//         type: "EXTENSION_RESPONSE",
+//         challenge: event.data.challenge,
+//         signature,
+//       },
+//       "*"
+//     )
+//   }
+// })
+
+window.hasLingolin = true // Global variable
+// OR: inject a hidden element
+document.body.appendChild(
+  Object.assign(document.createElement("div"), { id: "lingolin-marker" })
+)
