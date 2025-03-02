5a80f03bbb9836d8b996df9600f1f78f2b4c3001
Javi <javierezpeleta@gmail.com>
2025-03-02 17:38:55 +0100

    removed unused var + log

--- public/content.js
+++ public/content.js
@@ -21,8 +21,6 @@ document.body.appendChild(
   Object.assign(document.createElement("div"), { id: "lingolin-marker" })
 )
 
-let LOGGED_IN_USED_ID = false
-
 window.addEventListener("message", (event) => {
   if (event.source !== window || event.data.type !== "LINGOLIN_USER_ID") return
   // Send message to background script instead of directly using chrome.storage
@@ -30,7 +28,5 @@ window.addEventListener("message", (event) => {
     type: "LINGOLIN_USER_ID",
     data: event.data.data,
   })
-  LOGGED_IN_USED_ID = event.data.data.id
   console.log(" 🍎 ->->-> SENT to background:", event.data)
-  console.log(" 🍎 ->->-> SAVED as a var: " + LOGGED_IN_USED_ID)
 })
