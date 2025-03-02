0e93f6c61b3e102299d6b72759dd21b809b723e1
Javi <javierezpeleta@gmail.com>
2025-03-02 08:57:36 +0100

    feat: if not logged in, a button will let you go to lingolin.xyz to start your session from there

--- src/components/NeedToSignIn.tsx
+++ src/components/NeedToSignIn.tsx
@@ -1,5 +1,26 @@
+import BlurryEntranceFaster from "./BlurryEntranceFaster"
+import Title from "./Title"
+import { Button } from "./ui/button"
+
 const NeedToSignIn = () => {
-  return <div>NeedToSignIn</div>
+  return (
+    <div className="p-4 flex flex-col items-center justify-center py-20 gap-6">
+      <BlurryEntranceFaster>
+        <Title>No user session found</Title>
+      </BlurryEntranceFaster>
+      <BlurryEntranceFaster>
+        <Button
+          onClick={() => {
+            chrome.tabs.create({
+              url: "https://lingolin.xyz",
+            })
+          }}
+        >
+          Start Session
+        </Button>
+      </BlurryEntranceFaster>
+    </div>
+  )
 }
 
 export default NeedToSignIn
---
+++ src/components/Title.tsx
@@ -0,0 +1,5 @@
+const Title = ({ children }: { children: React.ReactNode }) => {
+  return <div className="text-2xl font-bold">{children}</div>
+}
+
+export default Title
