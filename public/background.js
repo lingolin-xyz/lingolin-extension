cbf85d476c8e6247d092a2e71252c83941af5f36
Javi <javierezpeleta@gmail.com>
2025-03-04 20:00:12 +0100

    feat: localhost inverted color icons for my own sanity lol

--- public/background.js
+++ public/background.js
@@ -7,3 +7,47 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     )
   }
 })
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url || tab.url) {
+    updateIcon(tab.url)
+  }
+})
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  let tab = await chrome.tabs.get(activeInfo.tabId)
+  updateIcon(tab.url)
+})
+
+function updateIcon(url) {
+  if (!url) {
+    console.log("No URL provided")
+    return
+  }
+
+  // Add more robust URL checking
+  try {
+    const urlObj = new URL(url)
+    console.log("Valid URL:", urlObj.hostname)
+
+    if (urlObj.hostname === "localhost" || urlObj.hostname === "127.0.0.1") {
+      chrome.action.setIcon({
+        path: {
+          16: "icons/localicon16.png",
+          48: "icons/localicon48.png",
+          128: "icons/localicon128.png",
+        },
+      })
+    } else {
+      chrome.action.setIcon({
+        path: {
+          16: "icons/icon16.png",
+          48: "icons/icon48.png",
+          128: "icons/icon128.png",
+        },
+      })
+    }
+  } catch (e) {
+    console.error("Invalid URL:", url, e)
+  }
+}
